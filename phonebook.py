2d81a69982191583ee9843991073611c5e40d12a
Zaimabintezia <Zaimazia68@gmail.com>
2025-04-26 21:57:27 +0600

    phonebook done

--- phonebook.py
+++ phonebook.py
@@ -69,6 +69,101 @@ def menu():
     choice = int(input("Enter your choice: "))
     return choice  
 
+def add_contacts(phonebook):
+    dip = []
+    for i in range(len(phonebook[0])):
+        if i == 0:
+            dip.append(input("Enter name: "))
+        if i ==1:
+            dip.append(input("Enter phone number: "))
+        if i == 2:
+            dip.append(input("Enter email: "))
+        if i == 3:
+            dip.append(input("Enter date of birth(mm/dd/yy): "))
+        if i == 4:
+            dip.append(input("Enter category (Family/Friends/Work/Others): "))
+
+    phonebook.append(dip)
+    return phonebook
+
+def remove_existing_contact(phonebooks):
+    query = input("Pleae enter the name of the contact you wish to remove: ")
+    temp = 0
+
+    for i in range(len(phonebooks)):
+        if query == phonebooks[i][0]:
+            temp += 1
+            print(phonebooks.pop(i))
+            print(f"Conacts has been removed for {query}")
+            return phonebooks
+    if temp == 0:
+        print("Sorry, you have entered the wrong contact name.")
+    return phonebooks
+
+def search_for_contact(phonebooks):
+    print("Enter search criteria: ")
+    print("1. Name")
+    print("2. Phoen Number")
+    print("3. Email")
+    print("4. Date of Birth (mm/dd/yy)")
+    print("5. Category (Family/Friends/Work/Others)")
+    choice = int("Enter your choice")
+    
+    temp = []
+    check = -1
+    if choice == 1:
+        query = input("Enter the name: ")
+        for i in range(len(phonebooks)):
+            if query == phonebooks[i][0]:
+                check = i
+                temp.append(phonebooks[i])
+
+    elif choice == 2:
+        query = input("Enter the phone number: ")
+        for i in range(len(phonebooks)):
+            if query == phonebooks[i][1]:
+                check = i
+                temp.append(phonebooks[i])
+    
+    elif choice == 3:
+        query = input("Enter the email: ")
+        for i in range(len(phonebooks)):
+            if query == phonebooks[i][2]:
+                check = i
+                temp.append(phonebooks[i])
+
+    elif choice == 4:
+        query = input("Enter the dob (mm/dd/yy): ")
+        for i in range(len(phonebooks)):
+            if query == phonebooks[i][3]:
+                check = i
+                temp.append(phonebooks[i])
+
+    elif choice == 5:
+        query = input("Enter the category (Family/Friends/Work/Others): ")
+        for i in range(len(phonebooks)):
+            if query == phonebooks[i][4]:
+                check = i
+                temp.append(phonebooks[i])  
+
+    else:
+        print("Invalid option: ")
+        return -1
+    if check == -1:
+        return check
+    else:
+        display_all(temp)
+        return check       
+        
+def display_all(phonebooks):
+    if not phonebooks:
+        print("No contatcs found")
+    else: 
+        for i in range(len(phonebooks)):
+            print(phonebooks[i])
+
+def delete_all(phonebooks):
+    return phonebooks.clear()
 
 def thanks():
     print(".............................................")
@@ -84,18 +179,22 @@ print(".............................................")
 
 choice = 1
 phonebooks = initial_phonebook()
-while choice in (1, 2, 3, 4, 5)
+while choice in (1, 2, 3, 4, 5):
     choice = menu()
     if choice == 1:
-        phonebooks = []
+        phonebooks = add_contacts(phonebooks)
     elif choice == 2:
-        phonebooks = []
+        phonebooks = remove_existing_contact(phonebooks)
     elif choice == 3:
-        phonebooks = []
+        row = search_for_contact(phonebooks)
+        if row == -1:
+            print("The contact does not exist.")
+        else:
+            print(f"The contact found at row {row+1}")
     elif choice == 4:
-        phonebooks = []
+        display_all(phonebooks)
     elif choice == 5:
-        phonebooks = []
+         delete_all(phonebooks)
     elif choice == 6:
         sys.exit()
     else: 
